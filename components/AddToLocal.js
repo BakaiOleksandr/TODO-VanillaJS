083e42db67d6112855b5462bc689558b1ba6f001
Oleksandr Bakai <saniabakay@gmail.com>
2025-12-14 20:15:53 +0100

    14/12/2025

--- components/AddToLocal.js
+++ components/AddToLocal.js
@@ -3,7 +3,7 @@ export function AddToLocal(inputValue) {
   //create local storage
   const localTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
   //add task to localStorage
-  localTasks.push({id:Date.now(),item:inputValue});
+  localTasks.push({id: Date.now(), item: inputValue});
   //save local Storage
   localStorage.setItem('tasks', JSON.stringify(localTasks));
 }
--- components/CheckLocalStg.js
+++ components/CheckLocalStg.js
@@ -1,5 +1,5 @@
-export function CheckLocalStg(){
+export function CheckLocalStg() {
   const localTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
 
   if (localTasks.length > 0) return true;
-};
+}
